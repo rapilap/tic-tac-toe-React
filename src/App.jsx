506031dc2b3da3fbb697217162c4bddd7c17b794
rapilap <raffyabdillah03@gmail.com>
2025-05-23 09:57:51 +0700

    add click (x) function

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
 import { useState } from "react";
 
-function App() {
-  return <></>;
+function Square() {
+  const [value, setvalue] = useState("");
+
+  function handleClick() {
+    setvalue("X");
+  }
+
+  return (
+    <button
+      onClick={handleClick}
+      className="w-9 h-9 border-solid border-2 border-black font-bold text-xl"
+    >
+      {value}
+    </button>
+  );
 }
 
-export default App;
+export default function Board() {
+  return (
+    <div className="flex flex-wrap items-center justify-center h-[108px] w-[108px]">
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+    </div>
+  );
+}
